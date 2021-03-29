4362a6b2182ac0ba73b639eb90d05a8f59e57f02
artevance <agustinus.michael29@gmail.com>
2021-03-29 16:40:59 +0700

    Add routing for submission

--- routes/ajax/index.php
+++ routes/ajax/index.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->namespace('Auth')->group(base_path('routes/ajax/auth.php'));
 Route::prefix('storage')->namespace('Storage')->group(base_path('routes/ajax/storage.php'));
+Route::prefix('jasmas')->namespace('Jasmas')->group(base_path('routes/ajax/jasmas.php'));
